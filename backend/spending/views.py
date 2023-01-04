da05ea85e032f952662a84b72a3c0dd424710edd
LeeJiWon <dlwldnjs1009@gmail.com>
2023-01-05 02:12:59 +0900

    member-> user 이름변경했습니다

--- backend/spending/views.py
+++ backend/spending/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+class SpendingView(APIView):
+    def get(self, request):
+        return Response("ok", status=200) #테스트용 Response
