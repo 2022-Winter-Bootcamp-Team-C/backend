f3486083671191f4f5455a444bfd767cb5c2c31c
dbrjs4594283 <dbrjs4594283@naver.com>
2023-01-06 02:00:13 +0900

    aaa
    bbb

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_auth',
-    'user.apps.UserConfig',
     'income',
     'user.apps.UserConfig',
     'spending',
