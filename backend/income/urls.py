3b49a07528df33e655504e2763410f366f3bec8e
EunJu JO <101851472+dmswn1004@users.noreply.github.com>
2023-01-09 22:01:30 +0900

    Revert "feat#39 오류 수정"

--- backend/income/urls.py
+++ backend/income/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list/<user_id>', views.get_income_list),
+    path('<user_id>', views.get_income_list),
     path('new/', views.post_new_income),
-    path('<income_id>', views.putnewIncome),
     path('total_income/<user_id>', views.get_income_this_month),
 ]
