4179af8c44b2306a4483414339d4d646b2fb1588
EunJu JO <dmswnehf@naver.com>
2023-01-09 21:27:02 +0900

    feat#39 C-1,3,4 수정2

--- backend/income/urls.py
+++ backend/income/urls.py
@@ -2,11 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
     path('list/<user_id>', views.getincomeList),
-=======
-    path('<user_id>', views.getincomeList),
->>>>>>> main
     path('new/', views.postnewIncome),
     path('<income_id>', views.putnewIncome)
 ]
