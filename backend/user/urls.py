95a1a2216d8c0e3fd58f59cc9634a411ba206330
LeeJiWon <dlwldnjs1009@gmail.com>
2023-01-11 20:35:09 +0900

    로그인/회원가입 수정, 비밀번호 타입 수정

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    # path('new', views.registration),
     path('new', views.join),
     path('login', views.login),
 ]
