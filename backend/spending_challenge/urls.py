2fcb7756147ceef875d49ea015c3b3662efb3859
dbrjs4594283 <dbrjs4594283@naver.com>
2023-01-15 16:00:31 +0900

    spending_challenge 로직 변경

--- backend/spending_challenge/urls.py
+++ backend/spending_challenge/urls.py
@@ -5,6 +5,6 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 urlpatterns = [
-    path('new/', views.post_sending_challenge_data),
+    path('new', views.post_sending_challenge_data),
     path('<user_id>', views.get_remaining_budget),
 ]
--- backend/spending_challenge/views.py
+++ backend/spending_challenge/views.py
@@ -16,22 +16,22 @@ from spending.models import Spending
 
 @api_view(['POST'])  # E-1 지출 챌린지 금액을 설정하면 DB에 저장
 def post_sending_challenge_data(request):
+    try:
+        bool(Spending_challenge.objects.get(user_id=request.data['user']))  # user_id 중복 체크, 중복인 경우 line30으로 이동
 
-    duplicate_check = bool(Spending_challenge.objects.get(user_id=request.data['user']))
-    if duplicate_check is True:
-        return JsonResponse({"message": "Duplicated_UserId"}, status=401)
+    except:  # 중복이 아닌 경우 실행
+        data = request.data
+        serializer = spending_challenge_post_serializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    data = request.data
-    serializer = spending_challenge_post_serializer(data=data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return JsonResponse({"message": "Duplicated_UserId"}, status=401)
 
 
 @api_view(['GET'])  # E-2 남은 챌린지 금액 반환
 def get_remaining_budget(request, user_id):
-
     challenge = Spending_challenge.objects.get(user_id=user_id)
     budget = challenge.budget
 
