49a3c774233c9d0e193c91135733fe9fa8164608
dbrjs4594283 <dbrjs4594283@naver.com>
2023-01-24 15:38:45 +0900

    ci test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,21 +9,24 @@ pymysql.install_as_MySQLdb()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-secret_file = os.path.join(BASE_DIR, 'secrets.json')  # secrets.json 파일 위치를 명시\
-with open(secret_file) as f:
-    secrets = json.loads(f.read())
+# secret_file = os.path.join(BASE_DIR, 'secrets.json')  # secrets.json 파일 위치를 명시\
+# with open(secret_file) as f:
+#     secrets = json.loads(f.read())
 
 
-def get_secret(setting):
-    try:
-        return secrets[setting]
-    except KeyError:
-        error_msg = "Set the {} environment variable".format(setting)
-        raise ImproperlyConfigured(error_msg)
+# def get_secret(setting):
+#     try:
+#         return secrets[setting]
+#     except KeyError:
+#         error_msg = "Set the {} environment variable".format(setting)
+#         raise ImproperlyConfigured(error_msg)
 
 
-OCR_SECRET_KEY = get_secret("SECRET_KEY")
-OCR_API_URL = get_secret("API_URL")
+# OCR_SECRET_KEY = get_secret("SECRET_KEY")
+# OCR_API_URL = get_secret("API_URL")
+
+OCR_SECRET_KEY = "SECRET_KEY"
+OCR_API_URL = "API_URL"
 
 DEBUG = True
 
